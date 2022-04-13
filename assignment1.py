85aa76a9206086a8bef9e4d4195113c1fd859059
Sahoo123408 <97968840+Sahoo123408@users.noreply.github.com>
2022-04-13 06:44:13 +0530

    Update and rename app.py to assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+import re
+
+String = """We test coders. Give us a try? Most welcome coder and having fun!this is really fun fun fun .Beautiful, is;better*than\nugly"""
+
+def Parse_paragraph(String):
+    for k in re.split('\.|\?|!|;|\n',String):
+        temp_dict = {}
+        for word in k.split():
+            if word in temp_dict:
+                temp_dict[word] +=1
+            else:
+                temp_dict[word] = 1
+                
+        if len(temp_dict) > 0 :
+            max_val = max(temp_dict.values())
+            line = k.strip()+'|'+str(len(temp_dict))+'|'+str(max_val)
+            print(line)
+
+def main():
+    Parse_paragraph(String)
+
+if __name__== "__main__":
+    main()
+
+
+    
